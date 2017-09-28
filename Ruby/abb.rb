fce268d116d505ba867824d7bf226131bcb3c595
alejandrorodriguezarias <alejandro.rodriguez.arias@udc.es>
2017-09-28 15:14:01 +0200

    Ruby en modulos y en ingles(probado y funcionando)

--- Ruby/abb.rb
+++ Ruby/abb.rb
@@ -1,253 +1,255 @@
-tNodeA = Struct.new(:key,:left,:right)
+module BinarySearchTreeDLP
+	TNODEA = Struct.new(:key,:left,:right)
 
-def error (error_mesage)
-	puts ("#{error_mesage}")
-	exit
-end #error message
+	def BinarySearchTreeDLP.error (error_mesage)
+		puts ("#{error_mesage}")
+		exit
+	end #error message
 
-def createNodeA (tNodeA)
-	new_node = tNodeA.new
-	if new_node == nil then
-		error(" *** abb.createNodeA: no memory avaliable")
-	end
-	return new_node
-end
-
-def emptyTree
-	return nil
-end; #emptyTree 
-
-#*********************************************************************
-#INSERT
-#*********************************************************************
-def iterativeInsert (tNodeA, tABB, key)	
-	#new node inicialization
-	new_node = createNodeA(tNodeA)
-	new_node.key = key
-	new_node.left = nil
-	new_node.right = nil
-	
-	if tABB == nil then
+	def BinarySearchTreeDLP.createNodeA
+		new_node = TNODEA.new
+		if new_node == nil then
+			error(" *** abb.createNodeA: no memory avaliable")
+		end
 		return new_node
-	else 
-		father = nil	
-		child = tABB
-		#Find insert position
-		while child != nil && child.key != key 
-			father = child
-			if key < child.key then
-				child = child.left
-			else
-				child = child.right
-			end
-		end	
-		
-		if child == nil then
-			if key < father.key then
-				father.left = new_node
-			else 
-				father.right = new_node
-			end
-		end #Duplicated keys ignored.
 	end
-	return tABB;
-end #Iterative Insert
 
-def recursiveInsert (tNodeA,tABB,key)
-	
-	if tABB == nil then
-		tABB = createNodeA(tNodeA)
-		tABB.key = key
-		tABB.left = nil
-		tABB.right = nil
-		
-	elsif key < tABB.key then
-		tABB.left = recursiveInsert(tNodeA,tABB.left,key)
-	elsif key > tABB.key then 
-		tABB.right = recursiveInsert(tNodeA,tABB.right,key)
-	end #Duplicated keys ignored
-	
-	return tABB;
-	
-end#recursiveInsert
-
-def keyInsert(tNodeA,tABB, key)
-	recursiveInsert(tNodeA,tABB,key)
-end#keyInsert
-
-#***********************************************************************
-
-def isEmptyTree(tABB)
-	return tABB == nil
-end #isEmptyTree
-
-def leftChild (tABB)
-	return tABB.left
-end #leftChild
-
-def rightChild(tABB)
-	return tABB.right
-end #rightChild
-
-def root(tABB)
-	return tABB.key
-end #root
-
-#*********************************************************************
-#Search
-#*********************************************************************
-def recursiveSearch (tABB,key)
-	if tABB == nil then
+	def BinarySearchTreeDLP.emptyTree
 		return nil
-	elsif tABB.key == key then
-		return tABB
-	elsif key < tABB.key then
-		return recursiveSearch(tABB.left,key)
-	else
-		return recursiveSearch(tABB.right,key)
-	end 
-end#recursiveSearch
+	end; #emptyTree 
 
-def iterativeSearch (tABB,key)
-	
-	node = tABB
-	while node != nil && node.key != key
-		if key < node.key then 
-			node = node.left
+	#*********************************************************************
+	#INSERT
+	#*********************************************************************
+	def BinarySearchTreeDLP.iterativeInsert (tBST, key)	
+		#new node inicialization
+		new_node = createNodeA
+		new_node.key = key
+		new_node.left = nil
+		new_node.right = nil
+		
+		if tBST == nil then
+			return new_node
 		else 
-			node = node.right
+			father = nil	
+			child = tBST
+			#Find insert position
+			while child != nil && child.key != key 
+				father = child
+				if key < child.key then
+					child = child.left
+				else
+					child = child.right
+				end
+			end	
+			
+			if child == nil then
+				if key < father.key then
+					father.left = new_node
+				else 
+					father.right = new_node
+				end
+			end #Duplicated keys ignored.
 		end
-	end
-	return node
-end #iterativeSearch
+		return tBST;
+	end #Iterative Insert
 
-def keySearch(tABB,key)
-	return recursiveSearch(tABB,key)
-end#recursiveSearch
-#********************************************************************
+	def BinarySearchTreeDLP.recursiveInsert (tBST,key)
+		
+		if tBST == nil then
+			tBST = createNodeA
+			tBST.key = key
+			tBST.left = nil
+			tBST.right = nil
+			
+		elsif key < tBST.key then
+			tBST.left = recursiveInsert(tBST.left,key)
+		elsif key > tBST.key then 
+			tBST.right = recursiveInsert(tBST.right,key)
+		end #Duplicated keys ignored
+		
+		return tBST;
+		
+	end#recursiveInsert
 
-#*********************************************************************
-#Delete
-#*********************************************************************
-def recursiveRemove(tABB,key)
-	
-	#when the target node have two children, this function it´s called
-	#Replace the target with the highest key node from left subtree
-	#father contains the father node from the highest key node from the left subtree
-	def rem2 (tree,father,aux)
-		if tree.right !=nil then
-			rem2(tree.right,tree,aux)
-		else 
-			aux.key = tree.key
-			#aux=tree 
-			#especial case if father.left = tree (father = aux)
-			if father.left == tree then 
-				father.left = tree.left
-			else
-				father.right = tree.left
-			end
-		end
-		return aux
-	end#sup2
-	
-	if tABB !=nil then 
-		if key < tABB.key then
-			tABB.left = recursiveRemove(tABB.left,key)
-		elsif key > tABB.key then
-			tABB.right = recursiveRemove(tABB.right,key)
-		else 
-			aux = tABB
-			if tABB.left == nil then
-				tABB=tABB.right
-			elsif tABB.right == nil then
-				tABB=tABB.left
-			else
-				tABB = rem2(tABB.left,tABB,aux)
-			end
-			aux = nil
-		end
-	end
-	return tABB
-end#recursiveRemove
+	def BinarySearchTreeDLP.keyInsert(tBST, key)
+		BinarySearchTreeDLP.recursiveInsert(tBST,key)
+	end#keyInsert
 
-def iterativeRemove (tABB,key)
+	#***********************************************************************
 
-	fDel = nil  #father of target node
-	del = tABB  #target node
-	#Search target node
-	while del != nil && del.key != key do
-		fDel = del
-		if key < del.key then 
-			del = del.left
+	def BinarySearchTreeDLP.isEmptyTree(tBST)
+		return tBST == nil
+	end #isEmptyTree
+
+	def BinarySearchTreeDLP.leftChild (tBST)
+		return tBST.left
+	end #leftChild
+
+	def BinarySearchTreeDLP.rightChild(tBST)
+		return tBST.right
+	end #rightChild
+
+	def BinarySearchTreeDLP.root(tBST)
+		return tBST.key
+	end #root
+
+	#*********************************************************************
+	#Search
+	#*********************************************************************
+	def BinarySearchTreeDLP.recursiveSearch (tBST,key)
+		if tBST == nil then
+			return nil
+		elsif tBST.key == key then
+			return tBST
+		elsif key < tBST.key then
+			return BinarySearchTreeDLP.recursiveSearch(tBST.left,key)
 		else
-			del = del.right
-		end
-	end
-	#If missing key do nothing
-	if del != nil then
-	
-		# Number of children
-		children = 0;
-		if del.left != nil then
-			children= children + 1
-		end
-		if del.right != nil then
-			children = children + 1
-		end
+			return BinarySearchTreeDLP.recursiveSearch(tBST.right,key)
+		end 
+	end#recursiveSearch
+
+	def BinarySearchTreeDLP.iterativeSearch (tBST,key)
 		
-		case children 
+		node = tBST
+		while node != nil && node.key != key
+			if key < node.key then 
+				node = node.left
+			else 
+				node = node.right
+			end
+		end
+		return node
+	end #iterativeSearch
+
+	def BinarySearchTreeDLP.keySearch(tBST,key)
+		return BinarySearchTreeDLP.recursiveSearch(tBST,key)
+	end#recursiveSearch
+	#********************************************************************
+
+	#*********************************************************************
+	#Delete
+	#*********************************************************************
+	def BinarySearchTreeDLP.recursiveRemove(tBST,key)
 		
-		when 0 
-			if fDel == nil then
-				tABB = nil # Root was the only node
-			elsif fDel.left == del then
-				fDel.left = nil
+		#when the target node have two children, this function it´s called
+		#Replace the target with the highest key node from left subtree
+		#father contains the father node from the highest key node from the left subtree
+		def BinarySearchTreeDLP.rem2 (tree,father,aux)
+			if tree.right !=nil then
+				BinarySearchTreeDLP.rem2(tree.right,tree,aux)
 			else 
-				fDel.right = nil
-			end  
-		#Delete node with one child
-		when 1
-			#NotEmptyChild is the not empty child from the node target
-			#Keep the not empty child
-			if del.left == nil then
-				notEmptyChild = del.right
+				aux.key = tree.key
+				#aux=tree 
+				#especial case if father.left = tree (father = aux)
+				if father.left == tree then 
+					father.left = tree.left
+				else
+					father.right = tree.left
+				end
+			end
+			return aux
+		end#sup2
+		
+		if tBST !=nil then 
+			if key < tBST.key then
+				tBST.left = BinarySearchTreeDLP.recursiveRemove(tBST.left,key)
+			elsif key > tBST.key then
+				tBST.right = BinarySearchTreeDLP.recursiveRemove(tBST.right,key)
 			else 
-				notEmptyChild = del.left
+				aux = tBST
+				if tBST.left == nil then
+					tBST=tBST.right
+				elsif tBST.right == nil then
+					tBST=tBST.left
+				else
+					tBST = BinarySearchTreeDLP.rem2(tBST.left,tBST,aux)
+				end
+				aux = nil
 			end
-			
-			# Delete target node
-			if fDel == nil then 
-				tABB = notEmptyChild	
-			elsif fDel.left == del then 
-				fDel.left = notEmptyChild
-			else
-				fDel.right = notEmptyChild
-			end
-		# Delete node with two children	
-		when 2
+		end
+		return tBST
+	end#recursiveRemove
+
+	def BinarySearchTreeDLP.iterativeRemove (tBST,key)
+
+		fDel = nil  #father of target node
+		del = tBST  #target node
+		#Search target node
+		while del != nil && del.key != key do
 			fDel = del
-			#subLeftMax node with highest key in left subtree
-			subLeftMax= del.left
-			
-			while subLeftMax.right != nil 
-				fDel = subLeftMax
-				subLeftMax = subLeftMax.right
-			end #while
-			
-			del.key = subLeftMax.key
-			
-			if fDel == del then 
-				fDel.left = subLeftMax.left
+			if key < del.key then 
+				del = del.left
 			else
-				fDel.right = subLeftMax.left
+				del = del.right
 			end
-			del = subLeftMax
-		end #case
-	end#if
-	return tABB
-end # iterativeRemove
+		end
+		#If missing key do nothing
+		if del != nil then
+		
+			# Number of children
+			children = 0;
+			if del.left != nil then
+				children= children + 1
+			end
+			if del.right != nil then
+				children = children + 1
+			end
+			
+			case children 
+			
+			when 0 
+				if fDel == nil then
+					tBST = nil # Root was the only node
+				elsif fDel.left == del then
+					fDel.left = nil
+				else 
+					fDel.right = nil
+				end  
+			#Delete node with one child
+			when 1
+				#NotEmptyChild is the not empty child from the node target
+				#Keep the not empty child
+				if del.left == nil then
+					notEmptyChild = del.right
+				else 
+					notEmptyChild = del.left
+				end
+				
+				# Delete target node
+				if fDel == nil then 
+					tBST = notEmptyChild	
+				elsif fDel.left == del then 
+					fDel.left = notEmptyChild
+				else
+					fDel.right = notEmptyChild
+				end
+			# Delete node with two children	
+			when 2
+				fDel = del
+				#subLeftMax node with highest key in left subtree
+				subLeftMax= del.left
+				
+				while subLeftMax.right != nil 
+					fDel = subLeftMax
+					subLeftMax = subLeftMax.right
+				end #while
+				
+				del.key = subLeftMax.key
+				
+				if fDel == del then 
+					fDel.left = subLeftMax.left
+				else
+					fDel.right = subLeftMax.left
+				end
+				del = subLeftMax
+			end #case
+		end#if
+		return tBST
+	end # iterativeRemove
 
-def removeKey(tABB,key)
-	recursiveRemove(tABB,key)
+	def BinarySearchTreeDLP.removeKey(tBST,key)
+		BinarySearchTreeDLP.recursiveRemove(tBST,key)
+	end
+
 end
-
--- Ruby/programa_abb.rb
+++ Ruby/programa_abb.rb
@@ -1,15 +1,17 @@
-def preorderDisplay (tABB) 
-	tABB = tABB
+require_relative 'abb'
+def preorderDisplay (tBST) 
+	tBST = tBST
 	
 	print ("(")
-	if !isEmptyTree(tABB) then 
-		if !isEmptyTree(leftChild(tABB)) || !isEmptyTree(rightChild(tABB)) then
-			print (" #{root(tABB)} ")
-			preorderDisplay(leftChild(tABB))
+	if !BinarySearchTreeDLP.isEmptyTree(tBST) then 
+		if !BinarySearchTreeDLP.isEmptyTree(BinarySearchTreeDLP.leftChild(tBST)) || 
+		!BinarySearchTreeDLP.isEmptyTree(BinarySearchTreeDLP.rightChild(tBST)) then
+			print (" #{BinarySearchTreeDLP.root(tBST)} ")
+			preorderDisplay(BinarySearchTreeDLP.leftChild(tBST))
 			print(" ")
-			preorderDisplay(rightChild(tABB))
+			preorderDisplay(BinarySearchTreeDLP.rightChild(tBST))
 		else
-			print (" #{root(tABB)} ")
+			print (" #{BinarySearchTreeDLP.root(tBST)} ")
 		end
 	end	
 	
@@ -17,42 +19,43 @@ def preorderDisplay (tABB)
 end 
 
 
-arbol = emptyTree
-arbol = keyInsert(tNodeA, arbol , 4)
-arbol = keyInsert(tNodeA, arbol , 4)
-arbol = keyInsert(tNodeA, arbol , 2)
-arbol = keyInsert(tNodeA, arbol , 6)
-arbol = keyInsert(tNodeA, arbol , 1)
-arbol = keyInsert(tNodeA, arbol , 3)
-arbol = keyInsert(tNodeA, arbol , 5)
-arbol = keyInsert(tNodeA, arbol , 7)
+tree = BinarySearchTreeDLP.emptyTree
+tree = BinarySearchTreeDLP.keyInsert(tree , 4)
+tree = BinarySearchTreeDLP.keyInsert(tree , 4)
+tree = BinarySearchTreeDLP.keyInsert(tree , 2)
+tree = BinarySearchTreeDLP.keyInsert(tree , 6)
+tree = BinarySearchTreeDLP.keyInsert(tree , 1)
+tree = BinarySearchTreeDLP.keyInsert(tree , 3)
+tree = BinarySearchTreeDLP.keyInsert(tree , 5)
+tree = BinarySearchTreeDLP.keyInsert(tree , 7)
+preorderDisplay(tree)
 
 puts()
 
-puts("buscar 1 .. #{root(keySearch(arbol,1))}")
-puts("buscar 2 .. #{root(keySearch(arbol,2))}")
-puts("buscar 3 .. #{root(keySearch(arbol,3))}")
-puts("buscar 4 .. #{root(keySearch(arbol,4))}")
-puts("buscar 5 .. #{root(keySearch(arbol,5))}")
-puts("buscar 6 .. #{root(keySearch(arbol,6))}")
-puts("buscar 7 .. #{root(keySearch(arbol,7))}")
+puts("buscar 1 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,1))}")
+puts("buscar 2 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,2))}")
+puts("buscar 3 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,3))}")
+puts("buscar 4 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,4))}")
+puts("buscar 5 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,5))}")
+puts("buscar 6 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,6))}")
+puts("buscar 7 .. #{BinarySearchTreeDLP.root(BinarySearchTreeDLP.keySearch(tree,7))}")
 
 print("removing 5...")		       
-removeKey(arbol, 5)
-preorderDisplay(arbol)
+BinarySearchTreeDLP.removeKey(tree, 5)
+preorderDisplay(tree)
 puts()
 
 print("removing 6...")		       
-removeKey(arbol, 6)
-preorderDisplay(arbol) 
+BinarySearchTreeDLP.removeKey(tree, 6)
+preorderDisplay(tree) 
 puts()
 
 print("removing 4...");			       
-removeKey(arbol, 4)
-preorderDisplay(arbol) 
+BinarySearchTreeDLP.removeKey(tree, 4)
+preorderDisplay(tree) 
 puts()
 
 print("removing 2...")			       
-removeKey(arbol, 2)
-preorderDisplay(arbol) 
+BinarySearchTreeDLP.removeKey(tree, 2)
+preorderDisplay(tree) 
 puts()
